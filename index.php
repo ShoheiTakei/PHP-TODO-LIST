76e37b5c930a2e7a6e8d5aeef091a5aede29e6d5
ShoheiTakei <mrshohei195@gmail.com>
2022-10-06 21:12:51 +0900

    環境構築

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Todo-List</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+    <h1>
+        TodoList
+    </h1>
+    <div class="main_section">
+        <?php echo "こんにちは" ?>
+    </div>
+ 
+
+</body>
+</html>
